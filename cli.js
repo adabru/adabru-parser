ad6c97b548772e3ab66902148c3c296b5ecfcca5
adabru <b.brunnmeier@gmx.de>
2017-03-01 23:38:43 +0100

    placeholder binary

---
+++ cli.js
@@ -0,0 +1,4 @@
+#!/usr/bin/env node
+
+console.log("Precompiled scripts are only available when installing with `npm install adabru-parser`. But this package seems to be installed directly from github. You can either install from npm registry or install webpack and livescript and run webpack in the package's folder.")
+console.log("\nHave a nice day :)")
