85cdd6fdcfa144f49f716f9b5be87347c310af2f
adabru <b.brunnmeier@gmx.de>
2018-10-30 17:15:23 +0100

    untrack dist/cli.js

--- dist/cli.js
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env node
-
-console.log("Precompiled scripts are only available from npm registry. But this package seems to be installed directly from github. You can either do `npm install adabru-parser` or `cd node_modules/adabru-parser && npm install`.")
-console.log("\nHave a nice day :)")
